3fa8d54821eaf9a28e82456a6a97a0f6624697a3
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-25 17:20:36 -0300

    Importo GSAP y animo el texto del hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,22 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import React from 'react'
 
 function Hero() {
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5
+    })
+  }, [])
+  
   return (
-    <div>Hero</div>
+    <section className='relative w-full bg-black nav-height'>
+      <div className='flex-col w-full h-5/6 flex-center'>
+        <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+      </div>
+    </section>
   )
 }
 
