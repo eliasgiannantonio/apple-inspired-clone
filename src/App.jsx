a1b29b17074ff63b7fa63e5cddf4ae5f46e3b48e
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-26 19:02:42 -0300

    Diseño de footer terminado

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Highlights from './components/Highlights'
 import Model from './components/Model'
 import Features from './components/Features'
 import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Model/>
       <Features/>
       <HowItWorks/>
+      <Footer/>
     </main>
   )
 }
