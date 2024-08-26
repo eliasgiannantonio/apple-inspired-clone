715c6f41e414a6c0598d0f0ab4d648a60af16921
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-26 18:49:19 -0300

    Sección how it works

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
 import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Highlights/>
       <Model/>
       <Features/>
+      <HowItWorks/>
     </main>
   )
 }
